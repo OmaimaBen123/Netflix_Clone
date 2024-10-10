b13f0d584bdc07b2efd97e53800009e4deeb3ce9
omaima <benhlima.omaima.solicode@gmail.com>
2024-10-10 21:47:26 +0100

    add home section and cards data

---
+++ src/assets/cards/cards_data.js
@@ -0,0 +1,88 @@
+import card_1 from './Inception.jpg'
+import card_2 from './the-dark.jpg';
+import card_3 from "./Interstellar.jpg";
+import card_4 from "./matrix.jpg";
+import card_5 from "./card4.jpg";
+import card_6 from "./titanic.jpg";
+import card_7 from "./card7.jpg";
+import card_8 from "./gladiator.jpg";
+import card_9 from "./park.jpg";
+import card_10 from "./Forest.jpg";
+import card_11 from "./father.jpg";
+import card_12 from "./redemption.jpg";
+import card_13 from "./park.jpg";
+import card_14 from "./pulp.jpg";
+const cards_data = [
+  {
+    
+    title: "Inception",
+    image: card_1
+  },
+  {
+
+    title: "The Dark Knight",
+    image: card_2
+  },
+  {
+   
+    title: "Interstellar",
+    image: card_3
+  },
+  {
+   
+    title: "The Matrix",
+    image: card_4
+  },
+  {
+   
+    title: "Avengers: Endgame",
+    image: card_5
+  },
+  {
+   
+    title: "Titanic",
+    image: card_6
+  },
+  {
+    
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    image: card_7
+  },
+  {
+    
+    title: "Gladiator",
+    image: card_8
+  },
+  {
+   
+    title: "Jurassic Park",
+    image: card_9
+  },
+  {
+    
+    title: "Forrest Gump",
+    image: card_10
+  },
+  {
+    
+    title: "The Godfather",
+    image: card_11
+  },
+  {
+    
+    title: "The Shawshank Redemption",
+    image: card_12
+  },
+  {
+    
+    title: "Fight Club",
+    image: card_13
+  },
+  {
+    
+    title: "Pulp Fiction",
+    image: card_14
+  }
+];
+
+export default cards_data;
